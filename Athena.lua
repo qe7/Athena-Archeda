181357bdfa38503acca8d6403de2d6b21f0d6bee
Shae <89613516+qe7@users.noreply.github.com>
2022-07-06 10:06:10 +0100

    Commit : removed weird commenting.
    People can't be that dumb... right?

--- Athena.lua
+++ Athena.lua
@@ -7,11 +7,9 @@ Literally just for me to dick around with and have fun with.
 local watermarkFont = render.create_font("Tahoma", 14, 500, 0)
 local height = engine.get_screen_height()
 local width = engine.get_screen_width()
---
 
 -- UI
 ui.checkbox("lua", "lua_output", "Enable watermark.", "enable_watermark_check_box")
---
 
 -- Functions
 local function drawWatermark()
@@ -21,8 +19,6 @@ local function drawWatermark()
         end
     end
 end
---
 
 -- Hooks
 hooks.add_to_hook("paint_traverse", drawWatermark)
---
